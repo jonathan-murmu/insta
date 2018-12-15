c12221953679a229faac5752ea01c10632971256
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-12-15 18:57:35 +0530

    logo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Logo from './components/Logo/Logo';
 class App extends Component {
   render() {
     return (
       <div className="">
-      
+      <Logo height="50px"/>
       </div>
+      
     );
   }
 }
