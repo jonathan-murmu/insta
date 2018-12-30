105e537ea758af79e8e93a18ce9c6a11f5224d75
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-12-30 15:14:13 +0530

    css fix

--- src/containers/Photogrid/Photogrid.js
+++ src/containers/Photogrid/Photogrid.js
@@ -8,35 +8,35 @@ import { Button } from 'reactstrap';
 class Photogrid extends Component {
     render() {
         return (
-            <div className="row">
+            <div className="row mt-5">
                 <div className="col-md-6 offset-md-3">
                     <div className="row">
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
-                        <div className="col-md-4 sm-12 pb-3">
-                            <Photo height="215px" width="215px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
+                        <div className="col-md-4 sm-12 p-2">
+                            <Photo height="215px" width="229px" grid="grid" src="https://s3-ap-southeast-1.amazonaws.com/he-public-data/insta_8fb860e0.jpeg"/>
                         </div>
                     </div>
                 </div>
