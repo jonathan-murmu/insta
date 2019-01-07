9cc9e4c36101807fedc0a89a041457381c4928ad
Jonathan <jonathan@propsamc.com>
2019-01-07 11:55:17 +0530

    profile summary base

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Photogrid from '../Photogrid/Photogrid';
+import ProfileSummary from '../ProfileSummary/ProfileSummary';
 
 class Home extends Component {
     render () {
         return (
             <div className="row mt-5 no-gutters">
                 <div className="col-md-6 offset-md-3">
+                    <ProfileSummary />
                     <Photogrid />
                 </div>
             </div>
---
+++ src/containers/ProfileSummary/ProfileSummary.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ProfileSummary extends Component {
+    render() {
+        return <div><h1>Profile Summary</h1></div>
+    }
+}
+
+export default ProfileSummary;
